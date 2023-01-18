3526d31302f2fc94dca2d61676bd69b8625897a9
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-01-18 17:57:37 +0200

    Рендер карток

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import simplelightbox from "simplelightbox";
 
 const refs = {
-form : document.getElementById('search-form'),
-input : document.querySelector('[type=text]'),
-submitBtn : document.querySelector('[type=submit]'),
-}
+  form: document.getElementById('search-form'),
+  input: document.querySelector('[type=text]'),
+  submitBtn: document.querySelector('[type=submit]'),
+  gallery: document.querySelector('.gallery'),
+};
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '32926436-c6ecc5f5fc4edb6e5a87cae88';
@@ -19,11 +20,14 @@ function onSubmitBtnClick(e) {
 
     return axios.get(`${BASE_URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`)
       .then(response => {
-          
         if (response.data.totalHits === 0) {
             Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         } else {
-            console.log(response);  
+          renderCard(response.data.hits)
+          refs.gallery.insertAdjacentHTML(
+            'beforeend',
+            renderCard(response.data.hits)
+          );
         }
       })
       .catch(error => {
@@ -32,4 +36,27 @@ function onSubmitBtnClick(e) {
    
 }
 refs.submitBtn.addEventListener("click", onSubmitBtnClick);
+
+
+function renderCard(arr) {
+  return arr.map(item => {
+    return `<div class="photo-card">
+      <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes${item.likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views${item.views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments${item.comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads${item.downloads}</b>
+        </p>
+      </div>
+    </div>`;
+  }).join('');
+}
 

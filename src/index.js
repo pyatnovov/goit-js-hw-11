182ec3c72699e67f6a7baa1f4a6a61e47ef0c268
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-01-20 22:45:37 +0200

    Готова робота

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// Описаний в документації
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import PictureService from './js/picture-service';
 import renderCard from './js/renderCard';
 
@@ -12,16 +12,42 @@ const refs = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 const pictureService = new PictureService();
+let lightbox = new SimpleLightbox('.photo-card a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 refs.submitBtn.addEventListener('click', onSubmitBtnClick);
 refs.loadMoreBtn.addEventListener('click', loadMore);
-
+refs.loadMoreBtn.classList.add('visually-hidden');
 function onSubmitBtnClick(e) {
-  e.preventDefault();
-  clearGallery();
-  pictureService.value = refs.input.value;
-  pictureService.resetPage();
-  pictureService.fetchPicture().then(apppendCardsMarkup);
+
+    e.preventDefault();
+
+    clearGallery();
+    pictureService.value = refs.input.value;
+
+    pictureService.resetPage();
+
+    lightbox.refresh();
+    pictureService.fetchPicture().then(array => {
+    if (array.length === 0) {
+    refs.loadMoreBtn.classList.add('visually-hidden');        
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+      pictureService.fetchPicture().then(apppendCardsMarkup);
+
+    if (pictureService.page === 2) {
+        refs.loadMoreBtn.classList.remove('visually-hidden');
+        Notify.success(
+          `Hooray! We found ${pictureService.totalHits} images.`
+        );
+      }
+
+    });
 }
 
 function loadMore() {
--- src/js/picture-service.js
+++ src/js/picture-service.js
@@ -1,26 +1,28 @@
 import axios from 'axios';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export default class pictureApiService {
   constructor() {
     this.inputValue = '';
     this.KEY = '32926436-c6ecc5f5fc4edb6e5a87cae88';
     this.BASE_URL = 'https://pixabay.com/api/';
     this.page = 1;
+    this.totalHits = 0;
   }
 
-  fetchPicture() {
-    return axiox.get(`${this.BASE_URL}?key=${this.KEY}&q=${this.inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
-      .then(response => {
-        if (response.data.totalHits === 0) {
-          Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        } else {
-          this.page += 1;
-          return response.data.hits;
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
+  async fetchPicture() {
+    const url = `${this.BASE_URL}?key=${this.KEY}&q=${this.inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
+
+    try {
+      const response = await axios.get(url);
+      const totalHits = response.data.totalHits;
+      this.incrementPage()
+      this.totalHits = totalHits;
+      return response.data.hits;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  incrementPage() {
+    this.page += 1;
   }
   resetPage() {
     this.page = 1;
